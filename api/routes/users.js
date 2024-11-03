e0915680f8db51b178a87a8964fb036da9c1e974
semih turan <semituran@gmail.com>
2024-11-03 13:35:03 +0300

    add user list

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const Users = require('../db/models/Users');
+const Response = require('../lib/Response');
+const CustomError = require('../lib/Error');
+const Enum = require('../config/Enum');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  try {
+    let users = await Users.find({});
+
+    res.json(Response.successResponse(users));
+
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err.message);
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
+
+router.post("/add", async (req, res) => {
+  let body = req.body;
+  try{
+    
+    if(!body.email) throw new CustomError(Enum.HTTP_CODES);
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err.message);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
 
 module.exports = router;
